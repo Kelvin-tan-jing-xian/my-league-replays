aead2df8b4a8fff73df975a8aca2fde83e580783
Kelvin-tan-jing-xian <77656965+Kelvin-tan-jing-xian@users.noreply.github.com>
2026-01-08 14:48:19 +0800

    added j4OutplayNasus

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
     { url: '/j4StealAtakhan.mp4', description: 'Waited in opponents bush side knowing they wont expect me to be there, Stole atakhan with smite and R', keywords: ['steal atakhan', 'stole atakhan','stealatakhan','j4 steal atakhan'] },
     { url: '/GoodUseOfRiftHerald.mp4', description: 'Saw all of enemy taking bot drag so I use herald on mid and got 2 inner turrets', keywords: ['herald push mid', 'rift herald push mid','rift herald mid push','push mid rift herald','2 mid turrets 1 inihibitor'] },
     { url: '/kaynSoloKillAmumu.mp4', description: 'invaded enemy top took his red quadrant and placed ward and saw amumu walking into me so I ambushed him knowing im stronger with red buff and potion and first to attack, main point was to farm red orbs', keywords: ['kayn amumu', 'invade red quadrant','kayn solo kill amumu'] },
+    { url: '/j4OutplayNasus.mp4', description: 'Nasus no flash so I knew as long as I cage him and E Q out he cant do shit, so I baited him into tower and caged and outplayed him', keywords: ['j4 outplay nasus', 'j4 cage nasus'] },
     // { url: '/kayggrgergregreg.mp4', description: 'gregergergergg', keywords: ['kayn', 'mundo','renekton','lose 4 waves','lvl 3 lvl 1'] },
   ];
 
